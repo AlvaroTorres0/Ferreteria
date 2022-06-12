8dd5c3ed7a276fab2111fc599f93a8c436668cdd
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2022-06-11 20:05:54 -0500

    modificar Aval

--- php/modificaciones/consultar_aval.php
+++ php/modificaciones/consultar_aval.php
@@ -1,47 +1,53 @@
 <?php
+
 include('../../conexion.php');
 //incluye el archivo php que contiene la conexion
 $conn=Conectar();//variable que almacena la conexión ala base de datos
+$noEmpleado=$_REQUEST['noEmpleado'];
+$sql = "SELECT * FROM ferreteria.aval WHERE noEmpleadoAval=" . "'" . $noEmpleado . "'";
 
 ?>
 
-<div class="col-12">
-        <table border="1">
-		<thead>
-		<tr>
-			<th> ID </th>
-			<th>Nombre</th>
-			<th>Apellido<br>Paterno</th>
-            <th>Apellido<br>Materno</th>            
-            <th>Telefono 1</th>
-            <th>Telefono 2</th>
-            <th>Municipio</th>            
-            <th>Colonia</th>
-            <th>Calle y <br>numero</th>
 
-
-			
-		</tr>
-		</thead>
 <?php
-foreach ($conn->query('SELECT * from aval') as $row){
-    ?>
-<tr>
-	<td><?php echo $row['noEmpleadoAval'] ?></td>
-    <td><?php echo $row['nombreAval'] ?></td>
-    <td><?php echo $row['apepatAval'] ?></td>
-    <td><?php echo $row['apematAval'] ?></td>    
-    <td><?php echo $row['telefono1Aval'] ?></td>
-    <td><?php echo $row['telefono2Aval'] ?></td>
-    <td><?php echo $row['municipioAval'] ?></td> 
-    <td><?php echo $row['coloniaAval'] ?></td>
-    <td><?php echo $row['calleynumAval'] ?></td>    
+foreach ($conn->query($sql) as $row){
+?>
 
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 
+tenemos que cambiar el nombre si no agarra el principal
+<form method="post" name="formDatosAval" id="AvalActualizar">
+                    <h2>Modifique los datos</h2>
+                    <div class="grid">
 
-</tr>
+                        <img src="img/id.png">
+                        <input type="text" name="num_empleado" id="num_empleado" placeholder="numero de empleado" value=<?php echo $row['noEmpleadoAval'] ?> />
+
+                        <img src="img/user.png">
+                        <input type="text" name="nombre" id="nombre" placeholder="Escribe tu nombre" value=<?php echo $row['nombreAval'] ?> />
+
+                        <img src="img/user.png">
+                        <input type="text" name="apepat" id="apepat" placeholder="1r Apellido" value=<?php echo $row['apepatAval'] ?> />
+                        <img src="img/user.png">
+                        <input type="text" name="apemat" id="apemat" placeholder="2nd Apellido" value=<?php echo $row['apematAval'] ?> />
+
+                        <img src="img/phone-call.png">
+                        <input type="Number" name="telefono1" id="telefono1" placeholder="Telefono 1" value=<?php echo $row['telefono1Aval'] ?> />
+                        <img src="img/phone-call.png">
+                        <input type="Number" name="telefono2" id="telefono2" placeholder="Telefono 2" value=<?php echo $row['telefono2Aval'] ?> />
+
+                        <img src="img/location.png">
+                        <input type="text" name="municipio" id="municipio" placeholder="Municipio" value=<?php echo $row['municipioAval'] ?> />
+
+                        <img src="img/location.png">
+                        <input type="text" name="colonia" id="colonia" placeholder="Colonia" value=<?php echo $row['coloniaAval'] ?> />
+
+                        <img src="img/location.png">
+                        <input type="text" name="calleynum" id="calleynum" placeholder="Calle y número" value=<?php echo $row['calleynumAval'] ?> />
+
 <?php
 	}
 ?>
-</table>
-</div> 
+
+</div>
+</form>
--- php/modificaciones/modificar_aval.php
+++ php/modificaciones/modificar_aval.php
@@ -3,27 +3,29 @@ $num_empleado=$_POST['num_empleado'];
 $nombre=$_POST['nombre'];
 $apepat=$_POST['apepat'];
 $apemat=$_POST['apemat'];
-$Telefono1=$_POST['Telefono1'];
-$Telefono2=$_POST['Telefono2'];
+$Telefono1=$_POST['telefono1'];
+$Telefono2=$_POST['telefono2'];
 $municipio=$_POST['municipio'];
 $colonia=$_POST['colonia'];
 $calleynum=$_POST['calleynum'];
 
 
 
-include('../../conexion.php');//incluye el archivo php que contiene la conexion
+include('../../conexion.php');
+//incluye el archivo php que contiene la conexion
 $conn=Conectar();//variable que almacena la conexión ala base de datos
 
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO ferreteria.aval (noEmpleadoAval, nombreAval,apepatAval,apematAval, telefono1Aval, telefono2Aval,municipioAval,coloniaAval , calleynumAval)
-        VALUES ('$num_empleado', '$nombre', '$apepat', '$apemat','$Telefono1','$Telefono2','$municipio','$colonia','$calleynum')";
-if (mysqli_query($conn, $sql)) {
-    echo "Nuevo registro insertado con éxito";
+$sql = "UPDATE aval SET noEmpleadoAval = '$num_empleado',nombreAval = '$nombre',apepatAval = '$apepat',apematAval = '$apemat',telefono1Aval = '$Telefono1',telefono2Aval = '$Telefono2',municipioAval = '$municipio',coloniaAval = '$colonia',calleynumAval = '$calleynum' WHERE noEmpleadoAval = '$num_empleado'";
+
+
+if(mysqli_query($conn, $sql)) {
+    echo "Modificado";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
